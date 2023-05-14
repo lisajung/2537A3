50fb1b62ddd2442d17ef75c2f3efdf2bff4a07f6
lisajung <lisajung998@gmail.com>
2023-05-14 02:07:47 -0700

    Add previous and next btn and highlight button

--- index.js
+++ index.js
@@ -1,72 +1,135 @@
+const PAGE_SIZE = 10;
+let currentPage = 1;
+let pokemons = [];
 
 const setup = async () => {
     console.log("Setup is running");
 
-    const result = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=810")
-    const pokemons = result.data.results;
+    const result = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=810");
+    pokemons = result.data.results;
 
-    //get 81 buttons
-    const PAGE_SIZE = 10
-    const numberOfButtons = Math.ceil(pokemons.length / PAGE_SIZE)
+
+    const numberOfButtons = Math.ceil(pokemons.length / PAGE_SIZE);
+
+    //add page buttons
     for (let i = 0; i < numberOfButtons; i++) {
         $("#paginationControls").append(`
-        <button type="button" class="btn btn-primary">${i + 1}</button>
-        `)
+      <button type="button" class="btn btn-primary">${i + 1}</button>
+    `);
     }
 
-    //add event listener to buttons
-    $("#paginationControls button").on("click", async(event) => {
-        $("#main").empty()
-    
-    const startingIndex = (event.target.innerText - 1) * PAGE_SIZE
-    const endIndex = startingIndex + PAGE_SIZE
-     const SplicedPokemons = result.data.results.slice(startingIndex, endIndex)
-     console.log(SplicedPokemons)
+    const setPage = (pageNumber) => {
+        $("#paginationControls button").removeClass("active");
+        $(`#paginationControls button:nth-child(${pageNumber + 1})`).addClass("active");
+        currentPage = pageNumber;
+        displayPokemons();
+        showOrHidePreviousAndNextButtons();
+    }
 
-    for (let i = 0; i < SplicedPokemons.length; i++) {
-        //pokemons.forEach(async (pokemon, index) => {
-        pokemon = SplicedPokemons[i]
-        index = i
+    // add event listener to buttons
+    $("#paginationControls button").on("click", (event) => {
+        currentPage = parseInt(event.target.innerText);
+        setPage(currentPage);
+    });
 
-        const pokemonResult = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-        console.log(pokemonResult);
 
-        // array of ability names
-        const abilityNames = pokemonResult.data.abilities.map((ability) => `<li>${ability.ability.name}</li>`);
+    $("#paginationControls").prepend(`
+    <button type="button" class="btn btn-primary" id="previousButton">Previous</button>
+  `);
 
-        // array of stats
-        const stats = pokemonResult.data.stats.map((stat) => `<li>${stat.stat.name}: ${stat.base_stat}</li>`);
+    // event listener to previous button
+    $("#previousButton").on("click", () => {
+        if (currentPage > 1) {
+            setPage(currentPage - 1);
+        }
+    });
 
-        // array of type names
-        const typeNames = pokemonResult.data.types.map((type) => `<li>${type.type.name}</li>`);
+ 
+    $("#paginationControls").append(`
+    <button type="button" class="btn btn-primary" id="nextButton">Next</button>
+  `);
 
+    // event listener to next button
+    $("#nextButton").on("click", () => {
+        if (currentPage < numberOfButtons) {
+            setPage(currentPage + 1);
+        }
+    });
+
+    // show the first page of pokemons on setup
+    displayPokemons();
+    showOrHidePreviousAndNextButtons();
+};
+
+const showOrHidePreviousAndNextButtons = () => {
+    const numberOfButtons = Math.ceil(pokemons.length / PAGE_SIZE);
+
+    if (currentPage == 1) {
+        $("#previousButton").hide();
+    } else {
+        $("#previousButton").show();
+    }
+
+    if (currentPage == numberOfButtons) {
+        $("#nextButton").hide();
+    } else {
+        $("#nextButton").show();
+    }
+};
+
+
+const displayPokemons = async () => {
+    $("#main").empty();
+
+    const startingIndex = (currentPage - 1) * PAGE_SIZE;
+    const endIndex = startingIndex + PAGE_SIZE;
+    const splicedPokemons = pokemons.slice(startingIndex, endIndex);
+
+    for (let i = 0; i < splicedPokemons.length; i++) {
+        const pokemon = splicedPokemons[i];
+
+        const pokemonResult = await axios.get(
+            `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+
+        const abilityNames = pokemonResult.data.abilities.map(
+            (ability) => `<li>${ability.ability.name}</li>`
+        );
+
+        const stats = pokemonResult.data.stats.map(
+            (stat) => `<li>${stat.stat.name}: ${stat.base_stat}</li>`
+        );
+
+        const typeNames = pokemonResult.data.types.map(
+            (type) => `<li>${type.type.name}</li>`
+        );
 
         $("#main").append(`
-        <div class="card" style="width: 18rem;">
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index + 1 + startingIndex}.png" class="card-img-top" alt="...">
+      <div class="card" style="width: 18rem;">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i + 1 + startingIndex}.png" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${pokemon.name}</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          
+
           <!-- Button trigger modal -->
           <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${pokemon.name}">
-              Pokemon details
+            Pokemon details
           </button>
 
           <!-- Modal -->
           <div class="modal fade" id="exampleModal${pokemon.name}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                  <div class="modal-content">
-                      <div class="modal-header">
-                          <h1 class="modal-title fs-5" id="exampleModalLabel">${pokemon.name}</h1>
-                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                      </div>
-                      <div class="modal-body">
-                      <p> Pokemon ID: ${pokemonResult.data.id}</p>
-                      <p>Abilities: ${abilityNames.join(" ")}</p>
-                      <p>Stats: ${stats.join(" ")}</p>
-                      <p>Types: ${typeNames.join(" ")}</p>
-                      </div>
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">${pokemon.name}</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <p> Pokemon ID: ${pokemonResult.data.id}</p>
+                  <p>Abilities: ${abilityNames.join(" ")}</p>
+                  <p>Stats: ${stats.join(" ")}</p>
+                  <p>Types: ${typeNames.join(" ")}</p>
+                </div>
                       <div class="modal-footer">
                           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                           <button type="button" class="btn btn-primary">Save changes</button>
@@ -80,12 +143,8 @@ const setup = async () => {
       </div>
         `)
     }
+};
 
-
-    
-
-    })
-}
 
 $(document).ready(setup)
 
